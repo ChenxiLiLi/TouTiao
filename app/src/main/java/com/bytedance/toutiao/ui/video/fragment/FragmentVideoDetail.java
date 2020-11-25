4027668d0cb77c63e3297d2eadc6c6bd1e0bc5cb
lzy <156153838@qq.com>
2020-11-25 13:43:36 +0800

    commit

--- app/src/main/java/com/bytedance/toutiao/ui/video/fragment/FragmentVideoDetail.java
+++ app/src/main/java/com/bytedance/toutiao/ui/video/fragment/FragmentVideoDetail.java
@@ -62,8 +62,10 @@ public class FragmentVideoDetail extends BaseFragment<VideoViewModel, FragmentVi
                         break;
                     case R.id.tv_comment_num:
                     case R.id.iv_comment:
-                        CommentDialog commentDialog = new CommentDialog();
-                        commentDialog.show(getChildFragmentManager(), "");
+                        if(showToLoginFragment()){
+                            CommentDialog commentDialog = new CommentDialog();
+                            commentDialog.show(getChildFragmentManager(), "");
+                        }
                         break;
                 }
             }
@@ -111,6 +113,14 @@ public class FragmentVideoDetail extends BaseFragment<VideoViewModel, FragmentVi
         });
 
 
+    }
+    public boolean showToLoginFragment() {
+        SharedPreferences sp = getActivity().getSharedPreferences("login", Context.MODE_PRIVATE);
+        if(null == (sp.getString("username", null))){
+            ToLoginfragment toLoginfragment = new ToLoginfragment();
+            toLoginfragment.show(getChildFragmentManager(), "");
+            return false;
+        }else return true;
     }
 
     private void initData(){
